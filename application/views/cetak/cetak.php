8353a2e4c658573f02a6b1c004196f0cf9cca10e
Boy Kurniawan <boykurniawan123@gmail.com>
2021-12-14 11:01:59 +0700

    - perbaikan cetak

--- application/views/cetak/cetak.php
+++ application/views/cetak/cetak.php
@@ -2,6 +2,14 @@
 header("Content-type: application/vnd-ms-excel");
 header("Content-Disposition: attachment; filename=Cetak_Jadwal_Transfer.xls");
 ?>
+<style type="text/css">
+	.num {
+	  mso-number-format:General;
+	}
+	.text{
+	  mso-number-format:"\@";/*force text*/
+	}
+</style>
 <table border="1">
 	<tr>
 		<td><b>No</b></td>
@@ -13,14 +21,18 @@ header("Content-Disposition: attachment; filename=Cetak_Jadwal_Transfer.xls");
 	</tr>
 	<?php
 	$no = 1;
-	 foreach ($this->db->get_where('jadwal_transfer', ['jadwal_transfer'=>$tanggal])->result() as $rw): ?>
+	$this->db->select('a.*');
+	$this->db->from('jadwal_transfer a');
+	$this->db->join('transaksi_investasi b', 'a.no_transaksi = b.no_transaksi', 'inner');
+	$this->db->where('a.jadwal_transfer', $tanggal);
+	 foreach ($this->db->get()->result() as $rw): ?>
 		<tr>
 			<td><?php echo $no ?></td>
 			<td><?php echo $rw->nama ?></td>
 			<td><?php echo get_data('member','id_member',$rw->id_member,'nama_bank') ?></td>
-			<td><?php echo get_data('member','id_member',$rw->id_member,'no_rekening') ?></td>
+			<td class="text"><?php echo get_data('member','id_member',$rw->id_member,'no_rekening') ?></td>
 			<td><?php echo $rw->jadwal_transfer ?></td>
-			<td><?php echo $rw->nominal ?></td>
+			<td class="num"><?php echo $rw->nominal ?></td>
 		</tr>
 	<?php $no++; endforeach ?>
 </table>
